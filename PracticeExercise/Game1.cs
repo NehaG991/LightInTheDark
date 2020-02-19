e23c73cb4eb4df633798f61f4f4a3aa3e13b1819
jtc4133 <jtc4133@IGMOH1375-17>
2020-02-19 11:08:31 -0500

    Epic line written

--- PracticeExercise/Game1.cs
+++ PracticeExercise/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace PracticeExercise
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// Nova = ded
     /// </summary>
     public class Game1 : Game
     {
