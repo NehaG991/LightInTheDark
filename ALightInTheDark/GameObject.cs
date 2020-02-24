588e0f61ec8bd06bf4bdda49bde30183e5d9a13a
Nova <jtc4133@rit.edu>
2020-02-24 17:48:59 -0500

    Added the GameObject and Player classes

---
+++ ALightInTheDark/GameObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ALightInTheDark
+{
+    class GameObject
+    {
+        //Fields
+        protected Rectangle position;
+        protected Texture2D texture;
+
+        //Properties
+        public int X
+        {
+            get
+            {
+                return position.X;
+            }
+            set
+            {
+                position.X = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return position.Y;
+            }
+            set
+            {
+                position.Y = value;
+            }
+        }
+
+        //Constructor
+        public GameObject(int x, int y, int width, int height, Texture2D image)
+        {
+            position = new Rectangle(x, y, width, height);
+            this.texture = image;
+        }
+
+
+        //Methods
+
+        //Draws the object
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, position, Color.White);
+        }
+    }
+}
---
+++ ALightInTheDark/Player.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ALightInTheDark
+{
+    class Player : GameObject
+    {
+
+        //Fields
+
+        //This determines if the player is on the ground or not
+        private bool grounded;
+
+        //This stores the previous keyboard input
+        private KeyboardState kbOld;
+
+        //Velocity
+        private int velocityX; //the speed the player is moving left/right
+        private int velocityY; //the speed the player is moving up/down
+        private int velocityXMax; //the max speed the player can move left/right
+        private int velocityXMin; //the min speed the player can move left/right
+        private int velocityYMax; //the max speed the player can move up/down
+        private int velocityYMin; //the min speed the player can move up/down
+
+        //Acceleration
+        private int accelX; //The rate at which the horizontal speed is changing
+        private int accelY; //The rate at which the vertical speed is changing
+
+
+        //Properties
+        public int VelocityX
+        {
+            get
+            {
+                return velocityX;
+            }
+            set
+            {
+                velocityX = value;
+            }
+        }
+        public int VelocityY
+        {
+            get
+            {
+                return velocityY;
+            }
+            set
+            {
+                velocityY = value;
+            }
+        }
+        public int AccelX
+        {
+            get
+            {
+                return accelX;
+            }
+            set
+            {
+                accelX = value;
+            }
+        }
+        public int AccelY
+        {
+            get
+            {
+                return accelY;
+            }
+            set
+            {
+                accelY = value;
+            }
+        }
+        public bool Grounded
+        {
+            get
+            {
+                return grounded;
+            }
+            set
+            {
+                grounded = value;
+            }
+        }
+
+        //Constructor
+        public Player(int x, int y, Texture2D texture)
+            : base(x, y, texture.Width, texture.Height, texture)
+        {
+            velocityX = 0;
+            velocityY = 0;
+            velocityXMax = 6;
+            velocityXMin = -6;
+            velocityYMax = 12;
+            velocityYMin = -12;
+            accelX = 0;
+            accelY = 0;
+
+        }
+
+
+        //Method
+
+        //manages the players movement, using the values of acceleration, as well as how fast the player is able to move
+        public void Movement(KeyboardState kb, bool grounded)
+        {
+
+            this.grounded = grounded;
+
+            //Updates position and velocity
+            X += velocityX;
+            Y += velocityY;
+            velocityX += accelX;
+            velocityY += accelY;
+
+            if (kbOld != null) //Prevents movement on the first frame, in order to have a value in kbOld
+            {
+                //Run and Jump update the x acceleration and y velocity
+                Run(kb);
+
+                //Activates jump method if the player presses a jump button
+                if (kb.IsKeyDown(Keys.W) || kb.IsKeyDown(Keys.Space))
+                    Jump();
+
+                //The player will accelerate downwards if in the air
+                if (!grounded)
+                {
+                    accelY = -1;
+                }
+            }
+            //STILL NEED TO ADD ACTUAL MOVEMENT
+
+            kbOld = kb;
+        }
+
+        //Reads user input, and determines whether the player should be accelerating. It is called by the movement manager
+        private void Run(KeyboardState kb)
+        {
+            //'flow' changes based on whether the player is in the air or not. If they are in the air, their movement is dulled.
+            int flow = 1;
+            if (grounded)
+                flow = 2; //when on the ground, movement is normal
+
+            //If one key is pressed, and not the other, the player will move. both keys being pressed results in no movement
+            if (kb.IsKeyDown(Keys.A) && !kb.IsKeyDown(Keys.D))
+            { //Accelerate left
+                accelX = -flow;
+            }
+            else if (kb.IsKeyDown(Keys.D) && !kb.IsKeyDown(Keys.A))
+            { //Accelerate Right
+                accelX = flow;
+            }
+            else //If the user is not pressing a movement button, They will accelerate in the opposite direction from the one they're moving in
+            {
+                if (velocityX > 0)
+                    accelX = -flow;
+                if (velocityX < 0)
+                    accelX = flow;
+            }
+        }
+
+        //When called, if the criteria are met, the player jumps.
+        private void Jump()
+        {
+            if (!kbOld.IsKeyDown(Keys.W) && !kbOld.IsKeyDown(Keys.Space) && grounded) //Makes sure that this is a fresh press, and that the player is on the ground before jumping
+            {
+                velocityY = velocityYMax; //This sets the player to the maximum upward velocity
+            }
+        }
+
+    }
+}
